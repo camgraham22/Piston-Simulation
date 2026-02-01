21b2736737b215c110e45200eb63f0f00af41d3e
camgraham22 <cgraham2200@gmail.com>
2026-02-01 12:17:02 -0600

    added throttle control, updated physics

--- physics.cpp
+++ physics.cpp
@@ -16,42 +16,35 @@ Piston::Piston(Vector2 size, float weight, float distance, int RPM) {
     pistonPosition = { crankpinPosition.x - pistonSize.x / 2, crankpinPosition.y - realConrodLength};
     pistonWeight = weight;
     maxTravelDistance = distance;
-    engineRPM = RPM;
+    acceleration = 0;
 }
 
 void Piston::updatePosition(Vector2 previousPistonPosition, Vector2 previousCrankpinPosition, Vector2 conrodVector, float &acceleration, float &conrodLength, float &conrodRotation) {
 
-    // velocity.y += GRAVITY * GetFrameTime();
-
     pistonPosition.y = pistonPosition.y + ( pistonPosition.y - previousPistonPosition.y ) + (acceleration * ( GetFrameTime() * GetFrameTime()));
-    // cout << "\nCrank before: " << crankpinPosition.x << ", " << crankpinPosition.y;
-    crankpinPosition.x = crankpinPosition.x + ( crankpinPosition.x - previousCrankpinPosition.x ) /*+ (acceleration * ( GetFrameTime() * GetFrameTime()))*/; 
-    crankpinPosition.y = crankpinPosition.y + ( crankpinPosition.y - previousCrankpinPosition.y )/* + (acceleration * ( GetFrameTime() * GetFrameTime()))*/;
+    crankpinPosition.x = crankpinPosition.x + ( crankpinPosition.x - previousCrankpinPosition.x ); 
+    crankpinPosition.y = crankpinPosition.y + ( crankpinPosition.y - previousCrankpinPosition.y );
 
     for (int i = 0; i < 5; i++) {
         conrodVector.x = pistonPosition.x - crankpinPosition.x + 50;
         conrodVector.y = pistonPosition.y - crankpinPosition.y;
+
         conrodLength = sqrt(conrodVector.x * conrodVector.x + conrodVector.y * conrodVector.y);
         conrodRotation = 90 + atan2(conrodVector.y, conrodVector.x) * 180 / PI;
         float dx = crankpinPosition.x - crankshaftPosition.x;
         float dy = crankpinPosition.y - crankshaftPosition.y;
         float currentRadius = sqrt( dx * dx + dy * dy);
         float radiusError = 50.0f / currentRadius;
-        // cout << ", Radius Error: " << radiusError;
         crankpinPosition.x = crankshaftPosition.x + dx * radiusError;
-        crankpinPosition.y = crankshaftPosition.y + dy * radiusError;   
+        crankpinPosition.y = crankshaftPosition.y + dy * radiusError;  
+
         float lengthError = realConrodLength - conrodLength;
-        // cout << "\nError: " << lengthError;
         Vector2 unitVector = { conrodVector.x / conrodLength, conrodVector.y / conrodLength };
-        // cout << "\nVector before: { " << unitVector.x << ", " << unitVector.y << " }";
         unitVector.x = unitVector.x * lengthError;
         unitVector.y = unitVector.y * lengthError;
-        // cout << "\nVector after: { " << unitVector.x << ", " << unitVector.y << " }";
         pistonPosition.y += unitVector.y * .95f;
-        // cout << "\nOld crank: " << crankpinPosition.x << ", " << crankpinPosition.y;
         crankpinPosition.x -= unitVector.x * .05f;
         crankpinPosition.y -= unitVector.y * .05f;
-        // cout << "\nNew crank: " << crankpinPosition.x << ", " << crankpinPosition.y;
     }
 
 }
@@ -61,15 +54,14 @@ void Piston::combustion(float force, float &acceleration) {
 
 }
 
-void Piston::draw(float timePassed, float conrodLength, float conrodRotation, bool combusting) {
+void Piston::draw(float timePassed, float conrodLength, float conrodRotation, bool combusting, float explosionRadius) {
     if (combusting) {
-        DrawCircle(pistonPosition.x + pistonSize.x / 2, crankshaftPosition.y - realConrodLength - pistonSize.y - 20, 50, RED);
+        DrawCircle(pistonPosition.x + pistonSize.x / 2, crankshaftPosition.y - realConrodLength - pistonSize.y - 20, explosionRadius, RED);
     }
     DrawRectangleV({ pistonPosition.x, pistonPosition.y - pistonSize.y + 20}, pistonSize, GRAY);
     Rectangle conrod = { pistonPosition.x + pistonSize.x / 2, pistonPosition.y, 20, realConrodLength };
     DrawRectanglePro(conrod, {10,0}, conrodRotation, GRAY);
     DrawCircle(crankshaftPosition.x, crankshaftPosition.y, 70, GRAY);
-    // cout << "\nPos: " << crankpinPosition.x << ", " << crankpinPosition.y;
     DrawCircle(crankpinPosition.x, crankpinPosition.y, 20, BLACK);
 }
 
